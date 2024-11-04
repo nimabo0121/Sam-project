ce7786ed4b9f6ea623404ee028497955f900d59c
nimabo0121 <nimabo0121@gmail.com>
2024-11-04 20:40:02 +0800

    功能切版規劃
    功能切版規劃

--- next-bs5-test-main/pages/journal/index.js
+++ next-bs5-test-main/pages/journal/index.js
@@ -1,9 +1,46 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 export default function Journal() {
   return (
     <>
-      <div>Journal</div>
+      <div className="container pt-5">
+        {/* 紀錄向量表 */}
+        <div
+          className="row"
+          style={{ color: 'black', background: '#cccccc', height: '350px' }}
+        >
+          <p style={{ color: 'black', background: 'red', height: '10%' }}>
+            年/3月/月/周/日navbar
+          </p>
+          <div className="row">紀錄向量表</div>
+        </div>
+
+        <div className="row" style={{ height: '50px' }}>
+          <div
+            className="col-9"
+            style={{ color: 'pink', background: '#eeeeee' }}
+          >
+            <h3>食品分類navbar</h3>
+          </div>
+          <div
+            className="col-3"
+            style={{ color: 'black', background: '#dddddd' }}
+          >
+            <h3>熱量計算name</h3>
+          </div>
+        </div>
+
+        {/* 食品list, 計算結果(存儲功能) */}
+        {/* 需要欄位 食物name, 熱量, 日期 */}
+        <div className="row" style={{ height: '600px' }}>
+          <div className="col-9" style={{ background: 'orange' }}>
+            <p>食品list內容</p>
+          </div>
+          <div className="col-3" style={{ background: 'blue' }}>
+            <p>計算內容</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
